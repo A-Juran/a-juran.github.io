3d75c51e1b8d52c7bc4e7830d40940d1cd844648
JuRan <2369668922@qq.com>
2024-07-08 01:00:28 +0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https:/astroBlog.github.io/',
+	site: 'https://a-juran.github.io/',
 	base: '/',
 	integrations: [mdx(), sitemap()],
 });
