77424b0d55be31fe86c0fa535afd643390fbfdba
JuRan <2369668922@qq.com>
2024-07-08 14:02:22 +0800

    update blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://a-juran.github.io/',
-	base: '/',
-	integrations: [mdx(), sitemap()],
+	integrations: [tailwind()],
 });
