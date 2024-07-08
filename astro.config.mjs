529f55db1ee06a6e45ba71276a882cc8943a63a3
JuRan <2369668922@qq.com>
2024-07-08 14:41:45 +0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://a-juran.github.io',
-	base: '/',
+	site: "https://a-juran.github.io",
+	base: "/",
 	integrations: [tailwind()],
 });
